5bfdc9be763c884557a398d5a3cc1715db2763d2
lokeshbs1994 <lokeshbs619@gmail.com>
2022-02-17 10:47:14 +0530

    ADDED: Using Switch Case Statement

--- src/main/java/com/bridgeLabz/EmpWageBuilderOOPS.java
+++ src/main/java/com/bridgeLabz/EmpWageBuilderOOPS.java
@@ -14,15 +14,19 @@ public class EmpWageBuilderOOPS {
     public void computeWage() {
         int empHrs=0;
         int empWage=0;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME) {
-            empHrs = 8;
-            System.out.println("Employee is Present Full time");
-        } else if(empCheck==IS_PART_TIME){
-            empHrs = 4;
-            System.out.println("Employee is Present Part time");
-        } else {
-            System.out.println("Employee is Absent");
+        int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case IS_PART_TIME:
+                empHrs = 4;
+                System.out.println("Employee is Present part-time");
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                System.out.println("Employee is Present full-time");
+                break;
+            default:
+                empHrs = 0;
+                System.out.println("Employee is Absent");
         }
         empWage=empHrs*EMP_RATE_PER_HOUR;
         System.out.println("Emp Wage: "+ empWage);
