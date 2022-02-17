7c175bbd3f2cac86a54d6d66065e04c0034640e4
lokeshbs1994 <lokeshbs619@gmail.com>
2022-02-17 10:32:53 +0530

    ADDED: Calculate daily wage

--- src/main/java/com/bridgeLabz/EmpWageBuilderOOPS.java
+++ src/main/java/com/bridgeLabz/EmpWageBuilderOOPS.java
@@ -1,19 +1,26 @@
 package com.bridgeLabz;
 
 public class EmpWageBuilderOOPS {
-    public static final int IS_FULL_TIME=1;
+    public static final int IS_FULL_TIME = 1;
+    public int EMP_RATE_PER_HOUR = 20;
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage builder Using OOPS concepts");
-        empAttendance();
+        EmpWageBuilderOOPS empWageBuilderOOPS = new EmpWageBuilderOOPS();
+        empWageBuilderOOPS.computeWage();
     }
 
-    public static void empAttendance() {
+    public void computeWage() {
+        int empHrs=0;
+        int empWage=0;
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if(empCheck==IS_FULL_TIME) {
-            System.out.println("Employee is Present");
-        }else {
+        if (empCheck == IS_FULL_TIME) {
+            empHrs = 8;
+            System.out.println("Employee is Present Full time");
+        } else {
             System.out.println("Employee is Absent");
         }
+        empWage=empHrs*EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage: "+ empWage);
     }
 }
