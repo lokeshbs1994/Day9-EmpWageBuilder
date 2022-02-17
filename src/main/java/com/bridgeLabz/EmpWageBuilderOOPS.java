08ac3e005e8ba4fe8e5ff235ab84692627b4dc29
lokeshbs1994 <lokeshbs619@gmail.com>
2022-02-17 10:38:37 +0530

    ADDED: Part time employee added

--- src/main/java/com/bridgeLabz/EmpWageBuilderOOPS.java
+++ src/main/java/com/bridgeLabz/EmpWageBuilderOOPS.java
@@ -2,6 +2,7 @@ package com.bridgeLabz;
 
 public class EmpWageBuilderOOPS {
     public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
     public int EMP_RATE_PER_HOUR = 20;
 
     public static void main(String[] args) {
@@ -13,10 +14,13 @@ public class EmpWageBuilderOOPS {
     public void computeWage() {
         int empHrs=0;
         int empWage=0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
         if (empCheck == IS_FULL_TIME) {
             empHrs = 8;
             System.out.println("Employee is Present Full time");
+        } else if(empCheck==IS_PART_TIME){
+            empHrs = 4;
+            System.out.println("Employee is Present Part time");
         } else {
             System.out.println("Employee is Absent");
         }
