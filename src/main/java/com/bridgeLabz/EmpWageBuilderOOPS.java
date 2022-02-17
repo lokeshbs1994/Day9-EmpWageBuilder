c7d7a5a9b295cd813690d8601ec92903c14eff61
lokeshbs1994 <lokeshbs619@gmail.com>
2022-02-17 12:57:59 +0530

    ADDED: wage for each companies

--- src/main/java/com/bridgeLabz/EmpWageBuilderOOPS.java
+++ src/main/java/com/bridgeLabz/EmpWageBuilderOOPS.java
@@ -3,26 +3,36 @@ package com.bridgeLabz;
 public class EmpWageBuilderOOPS {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
-    private int EMP_RATE_PER_HOUR;
-    private int NUM_OF_WORKING_DAYS;
-    private int MAX_HRS_IN_MONTH;
+    private final String COMPANY;
+    private final int EMP_RATE_PER_HOUR;
+    private final int NUM_OF_WORKING_DAYS;
+    private final int MAX_HRS_IN_MONTH;
+    private int TOTAL_EMP_WAGE;
 
-    public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage builder Using OOPS concepts");
-        EmpWageBuilderOOPS empWageBuilderOOPS = new EmpWageBuilderOOPS();
-        empWageBuilderOOPS.computeWage("AMAZON", 20, 20, 100);
-        empWageBuilderOOPS.computeWage("FLIPCART", 10, 15, 100);
-    }
-
-    public void computeWage(String company, int empRatePerHour, int numberOfWorkingDays, int maxHrs) {
+    public EmpWageBuilderOOPS(String company, int empRatePerHour, int numberOfWorkingDays, int maxHrs) {
+        this.COMPANY = company;
         this.EMP_RATE_PER_HOUR = empRatePerHour;
         this.NUM_OF_WORKING_DAYS = numberOfWorkingDays;
         this.MAX_HRS_IN_MONTH = maxHrs;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee Wage builder Using OOPS concepts");
+        EmpWageBuilderOOPS amazon = new EmpWageBuilderOOPS("AMAZON", 20, 20, 100);
+        amazon.computeWage();
+        System.out.println(amazon);
+        EmpWageBuilderOOPS flipcart = new EmpWageBuilderOOPS("FLIPCART", 10, 15, 100);
+        flipcart.computeWage();
+        System.out.println(flipcart);
+    }
+
+    public void computeWage() {
         int empHrs;
         int empWage = 0;
         int totalWorkingDays = 0;
         int totalEmpHrs = 0;
-        int totalEmpWage = 0;
+        int totalEmpWage;
         while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -39,9 +49,13 @@ public class EmpWageBuilderOOPS {
             totalEmpHrs += empHrs;
             empWage = empHrs * EMP_RATE_PER_HOUR;
             System.out.println("Day# " + totalWorkingDays + " Emp Hrs: " + empHrs + " Emp Wage: " + empWage);
-            totalEmpWage += empWage;
         }
         totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Total Emp Wage of an employee in " + company + " is " + totalEmpWage);
+        this.TOTAL_EMP_WAGE = totalEmpWage;
+        //System.out.println("Total Emp Wage of an employee in " + COMPANY + " is " + totalEmpWage);
+    }
+    @Override
+    public String toString() {
+        return "Total Emp Wage of an employee in " + COMPANY + " is " + TOTAL_EMP_WAGE;
     }
 }
