f83d28c948e0f93d96759f54b157f033116e9a9b
lokeshbs1994 <lokeshbs619@gmail.com>
2022-02-17 11:42:32 +0530

    ADDED: refactor code

--- src/main/java/com/bridgeLabz/EmpWageBuilderOOPS.java
+++ src/main/java/com/bridgeLabz/EmpWageBuilderOOPS.java
@@ -3,7 +3,7 @@ package com.bridgeLabz;
 public class EmpWageBuilderOOPS {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
-    private int EMP_RATE_PER_HOUR ;
+    private int EMP_RATE_PER_HOUR;
     private int NUM_OF_WORKING_DAYS;
     private int MAX_HRS_IN_MONTH;
 
@@ -16,13 +16,13 @@ public class EmpWageBuilderOOPS {
     public void computeWage(int empRatePerHour, int numberOfWorkingDays, int maxHrs) {
         this.EMP_RATE_PER_HOUR = empRatePerHour;
         this.NUM_OF_WORKING_DAYS = numberOfWorkingDays;
-        this.MAX_HRS_IN_MONTH=maxHrs;
-        int empHrs = 0;
+        this.MAX_HRS_IN_MONTH = maxHrs;
+        int empHrs;
         int empWage = 0;
-        int totalWorkingDays=0;
-        int totalEmpHrs=0;
+        int totalWorkingDays = 0;
+        int totalEmpHrs = 0;
         int totalEmpWage = 0;
-        while (totalEmpHrs <= MAX_HRS_IN_MONTH  && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
@@ -35,11 +35,12 @@ public class EmpWageBuilderOOPS {
                 default:
                     empHrs = 0;
             }
-            totalEmpHrs+=empHrs;
-            System.out.println("Day# " + totalWorkingDays + " Emp Hrs: " + empHrs);
+            totalEmpHrs += empHrs;
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            System.out.println("Day# " + totalWorkingDays + " Emp Hrs: " + empHrs + " Emp Wage: " + empWage);
             totalEmpWage += empWage;
         }
-        totalEmpWage=totalEmpHrs* EMP_RATE_PER_HOUR;
+        totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
         System.out.println("Total Emp Wage: " + totalEmpWage);
     }
 }
